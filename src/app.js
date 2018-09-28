6d127c78410acf33c2190bf3ee3e55809fe23443
Jen Strong <jenniferastrong@gmail.com>
2018-09-28 14:05:25 -0500

    Pull out Pet component from App component

--- src/app.js
+++ src/app.js
@@ -1,36 +1,37 @@
-const Pet = (props) => {
-    return React.createElement("div", {}, [
-        React.createElement("h1", {}, props.name),
-        React.createElement("h2", {}, props.animal), 
-        React.createElement("h2", {}, props.breed)
-    ]); 
-}
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 class App extends React.Component {
-    handleTitleClick() {
-        alert("You clicked the title");
-    }
-    render () {
-        return React.createElement("div", {}, [ 
-            React.createElement("h1", { onClick: this.handleTitleClick }, 'Adopt Me!'), 
-            React.createElement(Pet, {
-                name: "Luna",
-                animal: "Dog", 
-                breed: "Poodle"
-            }),   
-            React.createElement(Pet, {
-                name: "Pepper",
-                animal: "Bird", 
-                breed: "Cockatiel"
-            }), React.createElement(Pet, {
-                name: "Doink",
-                animal: "Cat", 
-                breed: "Mixed"
-            })
-        ])
-        
-    }
+  handleTitleClick() {
+    alert("You clicked the title");
+  }
+  render() {
+    return React.createElement("div", {}, [
+      React.createElement(
+        "h1",
+        {
+          onClick: this.handleTitleClick
+        },
+        "Adopt Me!"
+      ),
+      React.createElement(Pet, {
+        name: "Luna",
+        animal: "Dog",
+        breed: "Poodle"
+      }),
+      React.createElement(Pet, {
+        name: "Pepper",
+        animal: "Bird",
+        breed: "Cockatiel"
+      }),
+      React.createElement(Pet, {
+        name: "Doink",
+        animal: "Cat",
+        breed: "Mixed"
+      })
+    ]);
+  }
 }
 
-
-ReactDOM.render(React.createElement(App), document.getElementById('root'))
+render(React.createElement(App), document.getElementById("root"));
