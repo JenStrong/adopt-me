1feaffdc7ff0932c9079e5e32ee3d044d64c9d9b
Jen Strong <jenniferastrong@gmail.com>
2018-10-23 15:32:07 -0500

    Config Babel to remove class constructor function

--- src/Details.js
+++ src/Details.js
@@ -8,13 +8,9 @@ const petfinder = pf({
 });
 
 class Details extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      loading: true
-    };
-  }
+  gitstate = {
+    loading: true
+  };
 
   componentDidMount() {
     petfinder.pet
